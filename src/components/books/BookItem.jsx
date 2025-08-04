1a31ea430ee2ff7bae52a065b4e37fb25eb05014
Calet Devia <calebtius@gmail.com>
2025-08-03 20:17:35 -0500

    Added the BookFavBtn component (Moved the btn from BookItem to BookFavBtn)

--- src/components/books/BookItem.jsx
+++ src/components/books/BookItem.jsx
@@ -1,3 +1,5 @@
+import BookFavBtn from "./BookFavBtn.jsx";
+
 const BookItem = () => {
   return (
     <>
@@ -15,27 +17,8 @@ const BookItem = () => {
             consequuntur repellat quibusdam!
           </h3>
           <h4 className="max-h-16 overflow-auto">Ernest Hemingway</h4>
+          <BookFavBtn />
         </article>
-        <button className="flex items-center gap-1 p-2 bg-white border-2 rounded-lg border-gray-200 w-full cursor-pointer transition-all hover:border-yellow-400">
-          <svg
-            className="size-6 fill-yellow-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-          >
-            <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z" />
-          </svg>
-          Añadir a favoritos
-        </button>
-        <button className="flex items-center gap-1 p-2 bg-yellow-400 border-2 text-white rounded-lg border-yellow-400 w-full cursor-pointer transition-all hover:border-gray-500">
-          <svg
-            className="size-6 fill-white"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-          >
-            <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z" />
-          </svg>
-          Quitar de favoritos
-        </button>
       </li>
       <li className="bg-white w-fit list-none flex flex-col gap-2 items-center p-6 rounded-lg shadow-lg max-w-[228.775px] transition-transform cursor-pointer ease-in-out hover:scale-105">
         <img
@@ -51,27 +34,8 @@ const BookItem = () => {
             consequuntur repellat quibusdam!
           </h3>
           <h4 className="max-h-16 overflow-auto">Ernest Hemingway</h4>
+          <BookFavBtn />
         </article>
-        <button className="flex items-center gap-1 p-2 bg-white border-2 rounded-lg border-gray-200 w-full cursor-pointer transition-all hover:border-yellow-400">
-          <svg
-            className="size-6 fill-yellow-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-          >
-            <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z" />
-          </svg>
-          Añadir a favoritos
-        </button>
-        <button className="flex items-center gap-1 p-2 bg-yellow-400 border-2 text-white rounded-lg border-yellow-400 w-full cursor-pointer transition-all hover:border-gray-500">
-          <svg
-            className="size-6 fill-white"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-          >
-            <path d="M341.5 45.1C337.4 37.1 329.1 32 320.1 32C311.1 32 302.8 37.1 298.7 45.1L225.1 189.3L65.2 214.7C56.3 216.1 48.9 222.4 46.1 231C43.3 239.6 45.6 249 51.9 255.4L166.3 369.9L141.1 529.8C139.7 538.7 143.4 547.7 150.7 553C158 558.3 167.6 559.1 175.7 555L320.1 481.6L464.4 555C472.4 559.1 482.1 558.3 489.4 553C496.7 547.7 500.4 538.8 499 529.8L473.7 369.9L588.1 255.4C594.5 249 596.7 239.6 593.9 231C591.1 222.4 583.8 216.1 574.8 214.7L415 189.3L341.5 45.1z" />
-          </svg>
-          Quitar de favoritos
-        </button>
       </li>
     </>
   );
