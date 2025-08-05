32ffbd48f965846b759d1d96c851e0000f691254
Calet Devia <calebtius@gmail.com>
2025-08-04 21:07:50 -0500

    Completed the googleBooks API consume code

--- src/services/googleBooks.js
+++ src/services/googleBooks.js
@@ -1,11 +1,21 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
-async function googleBooks(bookName) {
+async function googleBooks(book, setBooks) {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${bookName}:keyes&key=${apiKey}`
+      `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${apiKey}`
     );
-  } catch {
+
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    const data = await response.json();
+    if (!data) return;
+
+    const books = data.items;
+
+    setBooks(books);
+    console.log(books);
+  } catch (e) {
     console.log(e);
   }
 }
