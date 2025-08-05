ff35824ea99a3bd59d0f11c6a0f4d47b867a1e96
Calet Devia <calebtius@gmail.com>
2025-08-04 21:10:17 -0500

    Saved the name when in the bookName and added more state variables in the customHook

--- src/components/books/BookItem.jsx
+++ src/components/books/BookItem.jsx
@@ -1,13 +1,38 @@
 import BookFavBtn from "./BookFavBtn.jsx";
 
-const BookItem = () => {
+const BookItem = ({ bookInfo = {} }) => {
+  const {
+    id,
+    title,
+    authors,
+    description,
+    publisher,
+    publishedDate,
+    previewLink,
+    thumbnail,
+    smallThumbnail,
+  } = bookInfo;
+
   return (
     <>
-      <li className="bg-white w-fit list-none flex flex-col gap-2 items-center p-6 rounded-lg shadow-lg max-w-[228.775px] transition-transform cursor-pointer ease-in-out hover:scale-105">
+
         <img
           className="rounded-lg object-fill w-full h-[279.625px]"
-          src="/src/assets/image/noBook.png"
+          src={thumbnail}
           alt="book-no-found"
+        />
+        <article className="flex flex-col gap-2">
+          <h3 className="font-semibold text-2xl max-h-16 overflow-auto">
+            {title}
+          </h3>
+          <h4 className="max-h-16 overflow-auto">{authors}</h4>
+        </article>
+
+      {/* <li className="bg-white w-fit list-none flex flex-col gap-2 items-center p-6 rounded-lg shadow-lg max-w-[228.775px] transition-transform cursor-pointer ease-in-out hover:scale-105">
+        <img
+          className="rounded-lg object-fill w-full h-[279.625px]"
+          src={thumbnail}
+          alt="book-img"
         />
         <article className="flex flex-col gap-2">
           <h3 className="font-semibold text-2xl max-h-16 overflow-auto">
@@ -18,25 +43,8 @@ const BookItem = () => {
           </h3>
           <h4 className="max-h-16 overflow-auto">Ernest Hemingway</h4>
         </article>
-          <BookFavBtn />
-      </li>
-      <li className="bg-white w-fit list-none flex flex-col gap-2 items-center p-6 rounded-lg shadow-lg max-w-[228.775px] transition-transform cursor-pointer ease-in-out hover:scale-105">
-        <img
-          className="rounded-lg object-fill w-full h-[279.625px]"
-          src="http://books.google.com/books/content?id=FlDehedEjV4C&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-          alt="book-no-found"
-        />
-        <article className="flex flex-col gap-2">
-          <h3 className="font-semibold text-2xl max-h-16 overflow-auto">
-            El viejo y el mar Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Nisi alias odio reprehenderit atque, neque dicta culpa veniam
-            quidem provident quia, fugiat ducimus soluta suscipit quasi ut ipsam
-            consequuntur repellat quibusdam!
-          </h3>
-          <h4 className="max-h-16 overflow-auto">Ernest Hemingway</h4>
-        </article>
-          <BookFavBtn />
-      </li>
+        <BookFavBtn />
+      </li> */}
     </>
   );
 };
--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -1,4 +1,12 @@
+import { ContextBooks } from "../../context/contextBooks.js";
+
+import BookItem from "./BookItem.jsx";
+
+import { useContext, useState } from "react";
+
 const BookList = () => {
+  const { books } = useContext(ContextBooks);
+
   return (
     <>
       <img
@@ -6,6 +14,46 @@ const BookList = () => {
         src="/src/assets/image/emptySearch.png"
         alt="no-search"
       />
+
+      {books.map(({ volumeInfo, id }) => {
+        const {
+          title = "Sin titulo",
+          authors = ["Sin autor"],
+          description = "Sin descripción",
+          publisher = "Sin editorial",
+          publishedDate = "Sin fecha",
+          previewLink = "about:blank",
+          imageLinks,
+        } = volumeInfo;
+
+        const {
+          thumbnail = "/src/assets/image/noBook.png",
+          smallThumbnail = "/src/assets/image/noBook.png",
+        } = imageLinks;
+
+        const bookInfo = {
+          id,
+          title,
+          authors,
+          description,
+          publisher,
+          publishedDate,
+          previewLink,
+          thumbnail,
+          smallThumbnail,
+        };
+
+        return (
+          <ul>
+            <li
+              key={id}
+              className="bg-white w-fit list-none flex flex-col gap-2 items-center p-6 rounded-lg shadow-lg max-w-[228.775px] transition-transform cursor-pointer ease-in-out hover:scale-105"
+            >
+              <BookItem bookInfo={bookInfo} />;
+            </li>
+          </ul>
+        );
+      })}
     </>
   );
 };
